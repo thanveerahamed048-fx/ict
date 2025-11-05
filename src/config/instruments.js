0961f61d4065953e47b0c610cd3eb1366a6834e3
thanveerahamed048-fx <thanveerahamed048@gmail.com>
2025-11-05 16:06:50 +0530

    Update instruments.js error

--- src/config/instruments.js
+++ src/config/instruments.js
@@ -6,7 +6,7 @@ export const FX_INSTRUMENTS = [
    { id: 'USDJPY', feed: 'finnhub', feedSymbol: 'OANDA:USD_JPY', pipSize: 0.01,   decimals: 3 },
   { id: 'AUDUSD', feed: 'finnhub', feedSymbol: 'OANDA:AUD_USD', pipSize: 0.0001, decimals: 5 },
  { id: 'USDCAD', feed: 'finnhub', feedSymbol: 'OANDA:USD_CAD', pipSize: 0.0001, decimals: 5 },
-   {id: 'XAUUSD',feed: 'finnhub',feedSymbol: 'OANDA:XAU_USD',pipSize: 0.1,  decimals: 2
+   {id: 'XAUUSD',feed: 'finnhub',feedSymbol: 'OANDA:XAU_USD',pipSize: 0.1,  decimals: 2}
 ];
 export const CRYPTO_INSTRUMENTS = [
   { id: 'BTCUSDT', feed: 'binance', feedSymbol: 'btcusdt', tick: 0.1,  decimals: 2 },
