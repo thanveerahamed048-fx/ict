3f1cf3cca0693f0838c14d5af883bfe2491eb867
thanveerahamed048-fx <thanveerahamed048@gmail.com>
2025-11-05 16:03:38 +0530

    Update instruments.js

--- src/config/instruments.js
+++ src/config/instruments.js
@@ -6,13 +6,8 @@ export const FX_INSTRUMENTS = [
    { id: 'USDJPY', feed: 'finnhub', feedSymbol: 'OANDA:USD_JPY', pipSize: 0.01,   decimals: 3 },
   { id: 'AUDUSD', feed: 'finnhub', feedSymbol: 'OANDA:AUD_USD', pipSize: 0.0001, decimals: 5 },
  { id: 'USDCAD', feed: 'finnhub', feedSymbol: 'OANDA:USD_CAD', pipSize: 0.0001, decimals: 5 },
-
+   {id: 'XAUUSD',feed: 'finnhub',feedSymbol: 'OANDA:XAU_USD',pipSize: 0.1,  decimals: 2
 ];
-//   { id: 'EURUSD', feed: 'finnhub', feedSymbol: 'OANDA:EUR_USD', pipSize: 0.0001, decimals: 5 },
-//   { id: 'GBPUSD', feed: 'finnhub', feedSymbol: 'OANDA:GBP_USD', pipSize: 0.0001, decimals: 5 },
-//   { id: 'USDJPY', feed: 'finnhub', feedSymbol: 'OANDA:USD_JPY', pipSize: 0.01,   decimals: 3 },
-//   { id: 'AUDUSD', feed: 'finnhub', feedSymbol: 'OANDA:AUD_USD', pipSize: 0.0001, decimals: 5 },
-//   { id: 'USDCAD', feed: 'finnhub', feedSymbol: 'OANDA:USD_CAD', pipSize: 0.0001, decimals: 5 },
 export const CRYPTO_INSTRUMENTS = [
   { id: 'BTCUSDT', feed: 'binance', feedSymbol: 'btcusdt', tick: 0.1,  decimals: 2 },
   { id: 'ETHUSDT', feed: 'binance', feedSymbol: 'ethusdt', tick: 0.01, decimals: 2 },
